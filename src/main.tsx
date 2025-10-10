12bba43a16c3a3f565f6456e91cc76f3da90771f
Shubham Hire <shubham.hire2006@gmail.com>
2025-10-10 18:31:41 +0530

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+
+//app renderer
 createRoot(document.getElementById("root")!).render(<App />);
+
+
